a7adbe62aa6ebc18506c77996c947e88cdcc983a
unknown <cogjava4452@iiht.tech>
2022-11-14 16:55:32 +0530

    added student class

---
+++ Spring-MVC/SpringCoreDemo/src/main/java/com/mvc/Student.java
@@ -0,0 +1,38 @@
+package com.mvc;
+
+public class Student {
+	private Integer studentId;
+	private String studentname;
+	private String adress;
+	
+	
+	public Student(Integer studentId, String studentname, String adress) {
+		super();
+		this.studentId = studentId;
+		this.studentname = studentname;
+		this.adress = adress;
+	}
+	
+	
+	public Integer getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentname() {
+		return studentname;
+	}
+	public void setStudentname(String studentname) {
+		this.studentname = studentname;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
+	
+
+}
